7e5ecdc121e3902a77adf559dfdfbfa4c385ec56
hk-rigacoding <hk.rigacoding@gmail.com>
2021-05-22 13:28:16 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace day_15_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.bilde = new System.Windows.Forms.PictureBox();
+            this.log = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.bilde)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 122);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Poga";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bilde
+            // 
+            this.bilde.Location = new System.Drawing.Point(12, 31);
+            this.bilde.Name = "bilde";
+            this.bilde.Size = new System.Drawing.Size(55, 55);
+            this.bilde.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bilde.TabIndex = 1;
+            this.bilde.TabStop = false;
+            this.bilde.Click += new System.EventHandler(this.bilde_Click);
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(12, 384);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.log.Size = new System.Drawing.Size(408, 122);
+            this.log.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(680, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.eXITToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // eXITToolStripMenuItem
+            // 
+            this.eXITToolStripMenuItem.Name = "eXITToolStripMenuItem";
+            this.eXITToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.eXITToolStripMenuItem.Text = "EXIT";
+            this.eXITToolStripMenuItem.Click += new System.EventHandler(this.eXITToolStripMenuItem_Click);
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(12, 324);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(680, 54);
+            this.progress.TabIndex = 5;
+            this.progress.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 518);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.bilde);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Bilžuks";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bilde)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox bilde;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXITToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progress;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day_15_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            //komand rindas argumenti
+            string[] args = Environment.GetCommandLineArgs();
+
+            InitializeComponent();
+        }
+
+        private void bilde_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog d = new OpenFileDialog();
+
+
+            //šis kods izpildīsies, kad klikšķināsim uz pogas !
+
+
+            //failu izvēles filtrs
+            //solo fails
+            //d.Filter = "Image files(*.png; *.jpeg; *bmp)|*.png;*jpeg;*.bmp|Custom files(a*.*)|a*.*|All files(*.*)|*.*";
+
+
+
+            //multi fails
+
+            //string[] masīvs, kas saturēs vairākus failu nosaukumus
+            //d.FileNames
+
+            d.Multiselect = true;
+            d.Filter = "Image files(*.png; *.jpeg; *bmp)|*.png;*jpeg;*.bmp|Custom files(a*.*)|a*.*|All files(*.*)|*.*";
+
+            d.ShowDialog();
+
+            //pilns ceļš līdz izvēlētajam failam
+            string fname = d.FileName;
+            string[] fnames = d.FileNames;
+
+            //ja dialogā nospiežam cancel, tad fname būs tukšs !
+
+            //ja fname ir tukšs, tad neko nedaram
+            if (fnames.Length == 0)
+            {
+                MessageBox.Show("Nav izvēlēta bilde @");
+                return;
+            }
+
+            //bilde.ImageLocation = fname;
+            //bilde.SizeMode
+
+            PictureBox pb = new PictureBox();
+
+
+
+
+            //1. instancēt tik PictureBox objektus, cik ir izvēlēti faili
+            //2. ielādējiet bildes ar bilde.load("filename")
+            //3. salieciet (nokoriģējiet) adekvātas Y lokacijas !!! - jānosaka bildes augstums ar bilde.Image.Height
+            //
+            /*
+            PictureBox pb = new PictureBox();
+
+            
+            pb.Size = new Size(100,100);
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            this.Controls.Add(pb);
+
+            this.Refresh();
+            */
+
+
+            //korekcijas cikls
+            //pb.Location = new Point(10, Y+???);
+
+
+            progress.Maximum = 50;
+            int x = bilde.Location.X;
+
+            for (int i = 0; i < 50; i++)
+            {
+                //for () loops katrai bildei šito !
+                //{
+                //mainam bildes atrašanās vietu : sākotnējai X + i
+                bilde.Location = new Point(x  + i, bilde.Location.Y);
+                bilde.Load(fname);
+
+                //atļaut apstrādāt citus notikumus, savādāk forma būs 'iefrīzojusi'
+                Application.DoEvents();
+
+                //gaidīt 100 milisekundes
+                Thread.Sleep(100);
+                //}
+
+                progress.Value = i;
+            }
+            
+
+            //bilde.Load(fname);
+
+
+
+            //variants 1
+            //ciklā failu apstrāde
+            //log.AppendText(fname);
+            //new line  - stringam pievieno speciālo simbolu
+            //log.AppendText(Environment.NewLine);
+
+
+            //variants 2
+            log.Lines = fnames;
+
+
+            //masīvs ar elementiem
+            //string pirmaa = log.Lines[0];
+
+
+            //kā viens strings
+            //log.Text
+
+
+
+            //pievienot vai instancēt SaveFileDialog
+            //[izveidot filtru]
+            //izsaukt ShowDialog()
+            //ievadīt faila nosaukumu, kuŗā saglabāt "log" datus
+            //saglabāt failā "log" datus.
+
+
+            //kā noteikt, ko lietotājs ir izvēlējies Dialogā ?
+
+            //if (dialogs.ShowDialog() == DialogResult.OK)
+            //nospiests OK
+            //else
+            //nospiests cancel
+
+
+
+            SaveFileDialog s = new SaveFileDialog();
+
+
+            s.Filter = "Image files(*.png; *.jpeg; *bmp)|*.png;*jpeg;*.bmp|Custom files(a*.*)|a*.*|All files(*.*)|*.*";
+
+
+           
+            if (s.ShowDialog() != DialogResult.OK)
+            {
+                MessageBox.Show("Nav izvēlēta bilde @");
+                return;
+            }
+           
+
+            //s.ShowDialog();
+
+            //pilns ceļš līdz izvēlētajam failam
+               string sfname = s.FileName;
+
+
+            //ja fails eksistē
+            //if (File.Exists(sfname))
+
+            //ja fails neeksistē
+            if (!File.Exists(sfname))
+            {
+                File.AppendAllText(sfname, "ADDED " + DateTime.Now.ToString() + " \n");
+                File.AppendAllLines(sfname, log.Lines);
+            } else
+            {
+
+                DialogResult rezultaats = MessageBox.Show("Vai gribi pārrakstīt esošu failu ?", "Apstiprini ...", MessageBoxButtons.YesNo);
+                    
+                 if (rezultaats == DialogResult.Yes)
+                {
+                    File.AppendAllText(sfname, "ADDED " + DateTime.Now.ToString() + " \n");
+                    File.AppendAllLines(sfname, log.Lines);
+                }
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eXITToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            DialogResult rezultaats = MessageBox.Show("Vai gribi iziet no programmas ?", "Apstiprini ...", MessageBoxButtons.YesNo);
+
+            if (rezultaats == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+            
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
